82fe0ba71e688cade829b51960cfa9c33c4bf4f8
lilbugsecret <lamhuyp05@gmail.com>
2026-01-08 15:46:04 +0700

    update

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 let text = "";
 let display = document.getElementById("display");
 text += "<table border = '1'>";
-for (let j = 1; j <= 10; j++) {
+for (let j = 1; j < 10; j++) {
   text += "<tr>";
 
   for (let i = 2; i < 10; i++) {
